71dee0552a0916ea3d9f37ee806b3240cf3866de
martinlaursen97 <martin_laursen21@hotmail.com>
2022-05-05 21:24:48 +0200

    load table

--- js/loadItems.js
+++ js/loadItems.js
@@ -1,15 +1,16 @@
 const url = "http://localhost:8080/api/item/bar/";
 
 async function loadItems(id){
-  const table = document.querySelector(".item-table");
+  const table = document.getElementById("myTable");
 
   await fetch(url + id).then(res => res.json()).then(items => {
     for (let i = 0; i < items.length; i++){
-      let row = document.createElement("tr");
-      let col = document.createElement("td");
-      col.innerText = "hej";
-      row.appendChild(col);
-      table.appendChild(row);
+      let row = "<tr>" +
+                  "<td>" + items[i].itemName + "</td>" +
+                  "<td>" + items[i].price + "</td>" +
+                  "<td><input value='" + items[i].amountNo + "'/></td>" +
+                "</tr>";
+      table.innerHTML += row;
     }
 
   });
