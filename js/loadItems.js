f73884e7a3289388e71f77225e1c95e78f4e91e0
Nicklas00 <nick553h@stud.kea.dk>
2022-05-10 14:02:21 +0200

    Delete button made

--- js/loadItems.js
+++ js/loadItems.js
@@ -1,5 +1,7 @@
 const url = "http://localhost:8080/api/items/bar/";
 
+
+
 async function loadItems(id){
 
   const table = document.getElementById("myTable");
@@ -17,11 +19,40 @@ async function loadItems(id){
                   "<td>" + items[i].itemName + "</td>" +
                   "<td>" + items[i].type.typeName + "</td>" +
                   "<td><input value='" + items[i].amountNo + "'/></td>" +
+                  "<td><button class='btn btn-outline-danger' id='delete-btn' onclick='deleteById(" + items[i].id + ")'>Delete </button></td>"
                 "</tr>";
       table.innerHTML += row;
     }
   });
 }
+
+async function deleteById(id){
+  const url = "http://localhost:8080/api/items/" + id
+  const fetchOptions = {
+    method: "delete",
+    headers:{
+      "content-Type": "application/json",
+    },
+  };
+  location.reload();
+  return fetch(url, fetchOptions);
+
+}
+
+/*function updateItems(){
+  let colCount = 3
+  cell = row.insertCell(colCount);
+  const pbUpdate = document.createElement("input");
+  pbUpdate.type = "button";
+  pbUpdate.setAttribute('value', 'Opdate storage');
+  pbUpdate.onclick = function () {
+    updateRow(amount_no, , rowCount, row, inp, length, description, releasedate, imagelink);
+  }
+  cell.appendChild(pbUpdate);
+
+}
+
+ */
 
 
 
