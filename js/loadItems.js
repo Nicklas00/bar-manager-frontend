ee23f4416db1695386ca4f02670696d35444ee36
martinlaursen97 <martin_laursen21@hotmail.com>
2022-05-11 23:37:21 +0200

    update items

--- js/loadItems.js
+++ js/loadItems.js
@@ -4,7 +4,7 @@ async function getItems(url) {
   return await fetch(url).then(res => res.json());
 }
 async function loadItems(url){
-let items = await getItems(url);
+  let items = await getItems(url);
 
   const table = document.getElementById("myTable");
 
@@ -15,23 +15,24 @@ let items = await getItems(url);
     child = table.lastElementChild;
   }
 
-    for (let i = 0; i < items.length; i++){
-      let row = "<tr>" +
-                  "<td>" + items[i].itemName + "</td>" +
-                  "<td>" + items[i].type.typeName + "</td>" +
-                  "<td><input value='" + items[i].amountNo + "' onchange='updateInput(this.value)'id='test'/></td>" +
-                  "<td><button class='btn btn-outline-danger' id='delete-btn' onclick='deleteById(" + items[i].id + ")'>Delete</button></td>"
-                "</tr>";
-      table.innerHTML += row;
+  for (let i = 0; i < items.length; i++){
+    let row = "<tr>" +
+                "<td>" + items[i].itemName + "</td>" +
+                "<td>" + items[i].type.typeName + "</td>" +
+                "<td><input value='" + items[i].amountNo + "' onchange='updateInput(this.value, "+i+")' id='test"+i+"'/></td>" +
+                "<td><button class='btn btn-outline-danger' id='delete-btn' onclick='deleteById(" + items[i].id + ")'>Delete</button></td>"
+              "</tr>";
+    table.innerHTML += row;
   }
+    localStorage.setItem("items", JSON.stringify(items));
 }
-function updateInput(input){
-  document.getElementById("test").value=input;
-  alert(input);
+function updateInput(input, i){
+  let id = "test" + i;
+  document.getElementById(id).setAttribute('value',input);
 }
-function loadItems2(id){
+async function loadItems2(id) {
   localStorage.setItem("barId", JSON.stringify(id));
-  loadItems(url + id);
+  await loadItems(url + id);
 }
 
 async function deleteById(id){
@@ -59,21 +60,35 @@ async function search() {
     url = "http://localhost:8080/api/items" + "?keyword=" + input.value + "&barId=" + barId + "&typeId=" + typeId;
   }
 
-  loadItems(url);
+  await loadItems(url);
 }
 function isEmpty (str){
   return !str.trim().length;
 }
 
-function updateItems(){
-  const putUrl = "http://localhost:8080/api/items/";
-  let barId = JSON.parse(localStorage.getItem("barId"));
+async function updateItems() {
+  const putUrl = "http://localhost:8080/api/items";
+
+  let items = JSON.parse(localStorage.getItem("items"));
+
   let table = document.getElementById("myTable");
-  for (let i in table.rows){
+  for (let i in table.rows) {
     let row = table.rows[i];
-    alert(row.cells[2].innerHTML)
+    let oldVal = items[i].amountNo;
+    let newVal = row.cells[2].children[0].value
+
+    if (oldVal != newVal) {
+      items[i].amountNo = newVal;
+      const fetchOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(items[i])
+      };
+      await fetch(putUrl, fetchOptions);
+    }
   }
-  let items =  {};
 }
 
 
